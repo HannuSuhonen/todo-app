0c43a345b1f92586ab1eaba3bc7398a282cb47cd
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-09-23 13:06:01 +0300

    WIP save todo items to localstorage

--- src/createTodo.js
+++ src/createTodo.js
@@ -1,28 +1,34 @@
 export function createTodoModal() {
     let modal = document.createElement("div")
+    let todoObj = {}
     modal.classList.add("modal")
-    let content = document.createElement("form")
+    let content = document.createElement("div")
     content.classList.add("modal-content")
 
     let titleinput = document.createElement("input")
     titleinput.type = "text"
     titleinput.placeholder = "Title"
-    titleinput.id = "title"
     content.appendChild(titleinput)
 
     let descinput = document.createElement("input")
     descinput.type = "text"
     descinput.placeholder = "Description"
-    descinput.id = "description"
+
     content.appendChild(descinput)
 
     let dateInput = document.createElement("input")
     dateInput.type = "date"
-    dateInput.id = "date"
     content.appendChild(dateInput)
 
     let addbutton = document.createElement("button")
     addbutton.textContent = "Add"
+    addbutton.addEventListener("click", () => {
+        todoObj.title = titleinput.value;
+        todoObj.descinput = descinput.value;
+
+        localStorage.setItem(titleinput.placeholder,JSON.stringify(todoObj))
+        modal.style.display = "none"
+    })
     content.appendChild(addbutton)
 
     modal.appendChild(content)
